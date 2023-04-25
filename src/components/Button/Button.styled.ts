750b87bc0827edbda3ac1a4ecc24fa8c8b217a2c
Anton Cristo <antoncristo92@gmail.com>
2023-04-25 11:37:18 +0300

    button component storybook aligned

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
+import { triadicPalette } from 'src/style';
+
+// Create a palette decorator to allow any user to create it's own triadic palette
+const testColorsMode = 'light';
+
 export const Button = styled.button`
+	cursor: pointer;
+	border: none;
 	border-radius: 6px;
-	color: purple;
+	margin: 0;
 	padding: 6px 8px;
+
+	font-weight: 600;
+	color: ${triadicPalette[`primary-${testColorsMode}`]};
+	background-color: ${triadicPalette[`secondary-${testColorsMode}`]};
+
+	:hover {
+		box-shadow: -3px 0px 16px -2px ${triadicPalette.ternary};
+	}
 `;
