f68c5a4e313134f962482b7ba9b47fe8f288f82c
Anton Cristo <57062626+antoncristo@users.noreply.github.com>
2023-05-18 14:12:57 +0300

    Feat/table infra cleanup (#45)
    * eslint updated and infile linter cleanups
    
    * export table types used in config creation
    
    * minor version bump

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,23 +1,22 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import React from 'react';
+import React, { type HTMLAttributes } from 'react';
 
 import { initTable } from './init';
 import { type Column } from './Table.types';
 
 import * as Styled from './Table.styled';
 
-export interface TableProps<T> {
+export interface TableProps<T> extends HTMLAttributes<HTMLDivElement> {
 	data: T[];
 	columns: Array<Column<T>>;
 }
 
-export const Table = <T extends Object>(props: TableProps<T>) => {
-	const { data, columns } = props;
+export const Table = <T extends object>(props: TableProps<T>) => {
+	const { data, columns, ...rest } = props;
 
 	const { rows, headers } = initTable(data, columns);
 
 	return (
-		<Styled.Table data-testid='table_test_id'>
+		<Styled.Table data-testid='table_test_id' {...rest}>
 			<div className='thead'>
 				<div className='header-row'>
 					{headers.map((header, index) => (
