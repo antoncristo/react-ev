f8e7d30c0482cf044bdeac8df0a193a1ce23c972
Anton Cristo <antoncristo92@gmail.com>
2023-06-13 10:47:50 +0300

    button test update

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { customRender } from 'src/tests/setup-tests';
 import { Button } from './button';
+import { vi } from 'vitest';
 
 describe('<Button/> component', () => {
 	const buttonLabel = 'Test Button';
 
 	test('renders correctly', () => {
-		const onClickHandler = () => null;
-		customRender(<Button onClick={onClickHandler}>{buttonLabel}</Button>);
+		customRender(<Button onClick={() => null}>{buttonLabel}</Button>);
+
 		const component = screen.getByTestId('button_test_id');
 		expect(component).toBeInTheDocument();
+	});
+
+	test('click event is fired when user click the button', () => {
+		const onClickHandler = vi.fn(() => null);
+		customRender(<Button onClick={onClickHandler}>{buttonLabel}</Button>);
+
+		const component = screen.getByTestId('button_test_id');
+		fireEvent.click(component);
+		expect(onClickHandler).toBeCalled();
 	});
 });
